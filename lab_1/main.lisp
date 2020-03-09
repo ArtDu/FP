5a869d0362b7c0146e81da8647399644cf141d7d
ArtDub <rusartdub@gmail.com>
2020-03-09 13:43:36 +0300

    fix warning and add report

--- lab_1/main.lisp
+++ lab_1/main.lisp
@@ -1,12 +1,3 @@
-(defun palindrome-p (n)
-  (if (< n 10)
-    t
-    (if (and (eq (take-left n) (take-right n)) (palindrome-p (cut-left (cut-right n) 0 0 ))
-    )
-    t
-    nil)))
-
-
 (defun take-left (n)
   (if (< n 10)
     n
@@ -27,4 +18,31 @@
           (expt 10 m) (rem n 10)))
       (1+ m))))
 
-(print (palindrome-p 1221))
+(defun palindrome-p (n)
+  (if (< n 10)
+    t
+    (if
+      (and
+        (eq (take-left n) (take-right n))
+        (palindrome-p (cut-left (cut-right n) 0 0 )))
+      t
+      nil)))
+      (defun take-left (n)
+        (if (< n 10)
+          n
+          (take-left (truncate n 10))))
+
+      (defun take-right (n)
+        (rem n 10))
+
+      (defun cut-right (n)
+        (truncate n 10))
+
+      (defun cut-left (n z m)
+        (if (< n 10)
+          z
+          (cut-left (truncate n 10)
+            (+ z
+              (*
+                (expt 10 m) (rem n 10)))
+            (1+ m))))
